e49856113a2da7936ed0eac1d4a58ffba47eb702
daksh1205 <dakshyt1205@gmail.com>
2026-01-04 21:38:03 +0530

    Remove constants.dart from version control and add to gitignore

--- lib/core/utils/constants.dart
+++
@@ -1,8 +0,0 @@
-class AppSecrets {
-  static const String apiKey = 'AIzaSyDzsSg9kJYM9X1xbuHrHpF3AprOSnvslAg';
-  static const String baseUrl =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
-  static const String blandApiKey =
-      "org_34a69812d4d1065d12e311e1c739ab5f2545933417e9fef25eb635bdaadd8b4ccb65fe26c8e6d6784e8b69";
-  static const String blandBaseUrl = "https://api.bland.ai/v1/calls";
-}
